df935ca1c2caeafdf37c543bfc253e8b58ed9554
Bram <warsocket@gmail.com>
2017-07-11 21:26:43 +0200

    Initial Commmit

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+iptables -F INPUT
+iptables -F OUTPUT
+iptables -F FORWARD
+
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+
+
+ip6tables -F INPUT
+ip6tables -F OUTPUT
+ip6tables -F FORWARD
+
+ip6tables -P INPUT DROP
+ip6tables -P OUTPUT DROP
+ip6tables -P FORWARD DROP
+
+ip6tables -A INPUT -i lo -j ACCEPT
+ip6tables -A INPUT -m state --state ESTABLISHED -j ACCEPT
+ip6tables -A INPUT -s fe80::/10 -j ACCEPT
+ip6tables -A INPUT -p ipv6-icmp -j ACCEPT
